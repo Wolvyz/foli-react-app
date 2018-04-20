9bedd42163c875b64cefe463b2cb1d0936c5451f
Ville Vartiainen <ville.vartiainen@tuup.fi>
2018-04-20 22:21:18 +0300

    chore: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import './App.css';
+import Header from './Header/Header';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        <Header />
+      </div>
+    );
+  }
+}
+
+export default App;
