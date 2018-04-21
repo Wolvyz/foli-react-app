9c3e54290de44c68cecb682840bff823e580473a
Ville Vartiainen <ville.vartiainen@tuup.fi>
2018-04-22 02:14:40 +0300

    feat: add grid

--- src/Body/Body.tsx
+++ src/Body/Body.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
+import { Container, Grid} from 'semantic-ui-react';
 
+import MapComponent from './components/MapComponent';
 import FormField from './components/SearchComponent';
 
 import './Body.css';
@@ -8,7 +10,20 @@ class Body extends React.Component {
     public render() {
         return (
             <div className="Body-wrapper">
-                <FormField />
+                <Container>
+                    <Grid>
+                        <Grid.Row>
+                            <Grid.Column width={8}>
+                                <MapComponent />
+                            </Grid.Column>
+                        </Grid.Row>
+                        <Grid.Row>
+                            <Grid.Column width={8}>
+                                <FormField />
+                            </Grid.Column>
+                        </Grid.Row>
+                    </Grid>
+                </Container>
             </div>
         );
     }
---
+++ src/Body/components/MapComponent.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+import './MapComponent.css';
+
+class MapComponent extends React.Component {
+    public render() {
+        return (
+            <div />
+        );
+    }
+}
+
+export default MapComponent;
