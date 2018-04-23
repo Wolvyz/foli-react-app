3e89f8f2379cd2713349f61315aa4b890251149d
Ville Vartiainen <ville.vartiainen@tuup.fi>
2018-04-23 13:38:52 +0300

    WIP: fetch nearby stops

--- src/Body/components/MapComponent.tsx
+++ src/Body/components/MapComponent.tsx
@@ -1,21 +1,57 @@
 import axios from 'axios';
+import * as lodash from 'lodash';
 import * as React from 'react';
 import { GoogleMap, Marker, withGoogleMap } from "react-google-maps";
 
 import './MapComponent.css';
 
-class MapComponent extends React.Component<{}, {}> {
+interface IState {
+    vehicleData: any[]
+}
+
+class MapComponent extends React.Component<{}, IState> {
     constructor(props: any) {
         super(props);
 
         this.state = {vehicleData: []};
     }
 
-
     public componentDidMount() {
-        axios.get('http://data.foli.fi/siri/vm/pretty').then(res => {
-            const vehicleData = res.data.result;
-            res.data.result && res.data.result.length > 0 ? this.setState({vehicleData}) : this.setState({vehicleData: []});
+        const reqOptions = {
+            url: 'https://api.digitransit.fi/routing/v1/routers/waltti/index/graphql',
+            method: 'POST',
+            data: `{
+                stopsByRadius(lat:60.4518, lon:22.2666, radius:500) {
+                    edges {
+                     node {
+                       stop {
+                           timezone
+                            code
+                            name
+                              lat
+                              lon
+                              zoneId
+                        }
+                      }
+                  }
+                }
+            } `,
+            timeout: 8000,
+            headers: {'Content-Type': 'application/graphql'}
+        };
+
+        axios.request(reqOptions).then(res => {
+            const vehicleDatas = lodash.get(res, 'data.data.stopsByRadius.edges', []).map(edge => {
+                return {
+                    code: edge.node.stop.code,
+                    lat: edge.node.stop.lat,
+                    lon: edge.node.stop.lon,
+                    name: edge.node.stop.name,
+                    timeZone: edge.node.stop.timezone,
+                    zoneId: edge.node.stop.zoneId
+                }
+            });
+            vehicleDatas.length > 0 ? this.setState({vehicleData: vehicleDatas}) : this.setState({vehicleData: []});
         }).catch(err => this.setState({vehicleData: []}));
     }
 
