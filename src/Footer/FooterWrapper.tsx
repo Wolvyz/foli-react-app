bf3c92ea66610c4227fdf26bc68a4ef4fdddf0a9
Ville Vartiainen <ville.vartiainen@tuup.fi>
2018-05-16 23:18:38 +0300

    fix: footer to bottom

--- src/Footer/FooterWrapper.tsx
+++ src/Footer/FooterWrapper.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { Container } from 'semantic-ui-react';
 
 import './FooterWrapper.css';
 
@@ -7,9 +6,7 @@ class FooterWrapper extends React.Component {
     public render() {
         return (
             <div className="Footer-wrapper">
-            <Container>
                 <p>(c) Ville Vartiainen 2018</p>
-            </Container>
             </div>
         );
     }
